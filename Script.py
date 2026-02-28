b9f958177776fb9940445b937f467d90abd3af67
astrrommc <astrrommc@gmail.com>
2026-02-28 22:09:47 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -23,7 +23,7 @@ class script(object):
 ‣ ʟᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org/download/releases/3.0/'>ᴘʏᴛʜᴏɴ 3</a> 
 ‣ ᴅᴀᴛᴀ ʙᴀsᴇ : <a href='https://www.mongodb.com/'>ᴍᴏɴɢᴏ ᴅʙ</a> 
 ‣ ʙᴏᴛ sᴇʀᴠᴇʀ : <a href='https://render.com'>ʀᴇɴᴅᴇʀ</a> 
-‣ BUILD STATUS : v1.0 [STABLE]</b>"""
+‣ ʙᴜɪʟᴅ ꜱᴛᴀᴛᴜꜱ : ᴠ1.0 [ꜱᴛᴀʙʟᴇ]</b>"""
 
     CLONE_ABOUT_TXT = """<b><blockquote>⍟───[ ᴍʏ ᴀʙᴏᴜᴛ ]───⍟</blockquote>
     
@@ -744,6 +744,7 @@ Example:- /set_caption 📕 File Name: {filename}
 
 
     
+
 
 
 
