74f32f7b70b3b79045b64601a6f3af759c4eebdc
astrrommc <astrrommc@gmail.com>
2026-03-01 19:58:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://ibb.co/0jcsdKFX')).split()
+PICS = (environ.get('PICS', 'https://ibb.co/dJpcdK6d')).split()
 
 
 # Admins & Users
