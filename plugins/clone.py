d071fc40e09738d449320e80e3a37823b0f0434b
astrrommc <astrrommc@gmail.com>
2026-03-05 22:29:40 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -1,9 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-# Clone Code Credit : YT - @Tech_VJ / TG - @VJ_Bots / GitHub - @VJBots
-
 import re
 from Script import script
 from info import API_ID, API_HASH, CLONE_MODE, LOG_CHANNEL
